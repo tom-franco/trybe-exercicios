41ef02ebe1491d90b0fc71b9de8be55ccb7c21d6
Tom Franco <tomfranco1993@gmail.com>
2023-01-23 18:58:37 -0300

    primeiro exercicios

---
+++ fundamentos/secao-03-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,53 @@
+const a = 10;
+const b = 5;
+
+soma =  a + b;
+console.log('A soma é: ' + soma);
+
+subtração = a - b
+console.log('A subtração é: ' + subtração);
+
+multiplicação = a * b;
+console.log("A multiplicação é: " + multiplicação);
+
+divisão = a / b;
+console.log("A divisão é :" + divisão);
+
+modulo = a % b;
+console.log("O módulo é : " + modulo);
+
+
+if (a > b) {
+    console.log('A é maior que b')
+}
+else {
+    console.log('B é maior que a')
+}
+
+const c = 20;
+if (a > b && a > c) {
+    console.log('A é maior que b e c')
+}
+else if (a < b && b > c) {
+    console.log('B é maior que a e c')
+}
+else {
+    console.log('C é maior que a e b')
+}
+
+if (a > 0 && b > 0)  {
+    console.log("Número positivo")
+}
+else if (a < 0 && b < 0) {
+    console.log("Número negativo")
+}
+else {
+    console.log("Número zero")
+}
+
+
+
+
+
+
+
