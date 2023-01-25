64744e273af055ed2b4bd88e3f6260651b5423bd
Tom Franco <tomfranco1993@gmail.com>
2023-01-25 18:33:32 -0300

    concluido exercicios ao vivo

---
+++ fundamentos/secao-03-javascript-primeiros-passos/dia-03-logica-de-programacao-e-algoritmos/aovivo5.js
@@ -0,0 +1,11 @@
+let Carolzita = 20
+let Flavio = 30
+let Noel = 40
+
+if (Carolzita < Flavio && Carolzita < Noel) {
+    console.log('Carolzita é a mais nova!')
+} else if (Flavio < Carolzita && Flavio < Noel) {
+    console.log('Flavio é o mais novo!')
+} else {
+    console.log('Noel é o mais novo!')
+}
