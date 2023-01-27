32d524e2e5805051e96fe457f9c99cfce2f2925c
Tom Franco <tomfranco1993@gmail.com>
2023-01-27 18:19:48 -0300

    iniciando parte 1 dos exercicios

---
+++ fundamentos/secao-03-javascript-primeiros-passos/dia-05-javascript-es6/script.js
@@ -0,0 +1,25 @@
+/* function userInfo() {
+    let userEmail = 'maria@email.com';
+  
+    // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+    
+  }
+  userInfo();
+
+  console.log(userEmail);
+ */
+/*   if (true) {
+    // inicio do escopo do if
+    let userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
+  }
+  console.log(userAge); // 20 */
+
+/*   var userName = 'Isabella';
+var userName = 'Lucas';
+console.log(userName); // Resultado: Lucas */
+
+const myName = 'Isabella';
+console.log(`Welcome ${myName}!`);
+ 
